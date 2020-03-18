a56f4426fffa3b3f83316fbe143a424a586a4128
Mr. Singh <mrsingh.entertainment@gmail.com>
2020-03-18 14:56:52 +0530

    Updates

---
+++ precache-manifest.6d1825a8380cdde6ee6107bdf1c67da3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "374a27767d2d0b3943e6ac73d41fea70",
+    "url": "/react-and-firebase-exercise-tracker/index.html"
+  },
+  {
+    "revision": "02fc864d612816705c20",
+    "url": "/react-and-firebase-exercise-tracker/static/css/2.1417c86a.chunk.css"
+  },
+  {
+    "revision": "68b33870f7eb98845248",
+    "url": "/react-and-firebase-exercise-tracker/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "02fc864d612816705c20",
+    "url": "/react-and-firebase-exercise-tracker/static/js/2.d084ba41.chunk.js"
+  },
+  {
+    "revision": "df8be702f0cc9d299c0ff42ca03d3e40",
+    "url": "/react-and-firebase-exercise-tracker/static/js/2.d084ba41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68b33870f7eb98845248",
+    "url": "/react-and-firebase-exercise-tracker/static/js/main.25c141db.chunk.js"
+  },
+  {
+    "revision": "9c511185d947204c196e",
+    "url": "/react-and-firebase-exercise-tracker/static/js/runtime-main.af64fd28.js"
+  }
+]);
